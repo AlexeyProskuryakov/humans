cd61d1c07390fe60838b98803b963e60226e7d4c
Proskuryakov Alesha <a.proskuryakov@propellerads.net>
2016-10-09 18:45:22 +0300

    wake up as blueprint

--- wsgi/server.py
+++ wsgi/server.py
@@ -27,7 +27,7 @@ from wsgi.rr_people.human import HumanConfiguration
 from wsgi.rr_people.posting.posts import PostsStorage
 from wsgi.rr_people.posting.posts_important import ImportantYoutubePostSupplier
 from wsgi.rr_people.posting.posts_sequence import PostsSequenceStore, PostsSequenceHandler
-from wsgi.wake_up import WakeUp
+from wsgi.wake_up.views import wake_up_app
 
 __author__ = '4ikist'
 
@@ -44,6 +44,9 @@ app = Flask("Humans", template_folder=cur_dir + "/templates", static_folder=cur_
 app.secret_key = 'foo bar baz'
 app.config['SESSION_TYPE'] = 'filesystem'
 
+app.register_blueprint(wake_up_app, url_prefix="/wake_up")
+
+print app.url_map
 
 def array_to_string(array):
     return " ".join([str(el) for el in array])
@@ -59,41 +62,11 @@ if os.environ.get("test", False):
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     toolbar = DebugToolbarExtension(app)
 
-wu = WakeUp()
-wu.start()
 
 @app.route("/time-now")
 def time_now():
     return tst_to_dt(time.time())
 
-
-@app.route("/wake_up/<salt>", methods=["POST"])
-def wake_up(salt):
-    return jsonify(**{"result": salt})
-
-
-@app.route("/wake_up/check")
-def wake_up_check():
-    wu.check()
-    urls = map(lambda x: {"url": x.get("url"), "state": x.get("state")}, wu.store.get_urls_info())
-    return jsonify({"ok": True, "urls": urls})
-
-
-@app.route("/wake_up", methods=["GET", "POST"])
-def wake_up_manage():
-    if request.method == "POST":
-        urls = request.form.get("urls")
-        urls = filter(lambda x: x, map(lambda x: x.strip(), urls.split("\n")))
-
-        stored_urls = wu.store.get_urls()
-        to_delete = set(stored_urls).difference(urls)
-        deleted_count = wu.store.delete_urls(to_delete)
-        log.info("Delete %s urls" % deleted_count)
-        for url in urls:
-            wu.store.add_url(url)
-
-    urls = wu.store.get_urls()
-    return render_template("wake_up.html", **{"urls": urls})
 
 
 login_manager = LoginManager()
@@ -221,10 +194,6 @@ def logout():
 @app.route("/")
 @login_required
 def main():
-    if request.method == "POST":
-        _url = request.form.get("url")
-        wu.what = _url
-
     user = g.user
     return render_template("main.html", **{"username": user.name})
 
